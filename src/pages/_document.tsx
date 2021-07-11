a7ef305f7fa6df00e812aff0f74ae376115761cf
maxime beuil <maxime.beuil@algoan.com>
2021-07-11 22:13:16 +0200

    refactor(favicon): add multiple size / browser favicon, add to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,29 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicons/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicons/favicon-16x16.png"
+            sizes="16x16"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/favicons/apple-touch-icon.png"
+            sizes="180x180"
+          />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#14191e"
+          />
           <meta name="theme-color" content="#14191e" />
           <meta name="color-scheme" content="dark light" />
         </Head>
