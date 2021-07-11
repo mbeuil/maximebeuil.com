aa30d753330177f6917847a674eb2ab92e91c417
maxime beuil <maxime.beuil@algoan.com>
2021-07-11 14:40:17 +0200

    refactor(nav-bar): add logo button accessibility

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import NextLink from 'next/link';
+import { useI18n } from 'next-localization';
 
 import { Logo } from '@/icons';
 import LanguageButton from '@/components/language-button';
@@ -7,13 +8,17 @@ import ThemeButton from '@/components/theme-button';
 
 const NavBar: React.FC = () => {
   const [mounted, setMounted] = React.useState(false);
+  const i18n = useI18n();
 
   React.useEffect(() => setMounted(true), []);
 
   return (
     <div className="absolute max-w-5xl flex flex-row w-full mt-5 items-center gap-3 px-5">
       <NextLink href="/" passHref>
-        <button className="text-primary-1 hover:text-primary-2 mr-auto">
+        <button
+          aria-label={i18n.t('nav.logo')}
+          title={i18n.t('nav.logo')}
+          className="text-primary-1 hover:text-primary-2 mr-auto">
           <Logo />
         </button>
       </NextLink>
