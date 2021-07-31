e72ca110c041d162c96b8854774d54257c009b66
maxime beuil <maxime.beuil@algoan.com>
2021-07-31 20:25:39 +0200

    refactor(blog-card): rework card component

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -1,9 +1,32 @@
+import * as React from 'react';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 
 import Anchor from '@/components/anchor';
 import { Information, Language } from '@/models';
 import { formatDate } from '@/utils';
+import { useWidth } from '@/hooks';
+
+interface CardTitleProps {
+  number: number;
+  title: string;
+  isHover: boolean;
+}
+
+function CardTitle({ number, title, isHover }: CardTitleProps): JSX.Element {
+  return (
+    <div className="flex items-center gap-1">
+      <span
+        aria-hidden
+        className={`pt-1 font-mono text-2xl font-thin sm:text-3xl ${
+          isHover ? 'text-blue-2' : 'text-blue-1'
+        }`}>
+        {number + '.'}
+      </span>
+      <h3 className="text-2xl font-bold sm:text-3xl text-primary">{title}</h3>
+    </div>
+  );
+}
 
 interface BlogCardProps extends Information {
   index: number;
@@ -18,6 +41,8 @@ function BlogCard({
   slug,
 }: BlogCardProps): JSX.Element {
   const { locale } = useRouter();
+  const { width } = useWidth();
+  const [isHover, setIsHover] = React.useState(false);
 
   const date = formatDate(
     locale === Language.EN ? 'en-US' : 'fr-FR',
@@ -25,21 +50,30 @@ function BlogCard({
   );
 
   const number = index + 1;
-  const articlesNumber =
-    number < 10 ? `0${number.toString()}` : number.toString();
 
   return (
     <NextLink href={`/blog/${slug}`} passHref>
-      <Anchor className="flex flex-row items-center w-full pb-3 border-b border-secondary">
-        <div className="flex flex-col w-full">
-          <div className="flex items-center">
-            <span
-              aria-hidden
-              className="mr-1 font-mono text-2xl font-thin text-blue-1">
-              {articlesNumber}.
-            </span>
-            <h3 className="text-2xl font-bold text-primary">{title}</h3>
-          </div>
+      <Anchor
+        className="flex flex-row items-center w-full gap-2 mt-2 border-b border-secondary"
+        onMouseOver={() => setIsHover(true)}
+        onMouseOut={() => setIsHover(false)}>
+        {width >= 950 && (
+          <span
+            aria-hidden
+            className={`py-4 font-mono font-thin text-8xl ${
+              isHover ? 'text-blue-2' : 'text-blue-1'
+            }`}>
+            {number}
+          </span>
+        )}
+        <div className="flex flex-col w-full py-4">
+          {width >= 950 ? (
+            <h3 className="text-2xl font-bold sm:text-3xl text-primary">
+              {title}
+            </h3>
+          ) : (
+            <CardTitle number={number} title={title} isHover={isHover} />
+          )}
           <div className="bottom-0 flex w-full mb-1 font-mono text-sm">
             {tags.map((tag, id) => (
               <p key={id} className="text-secondary">
@@ -48,7 +82,7 @@ function BlogCard({
             ))}
             <p className="ml-auto text-secondary">{date}</p>
           </div>
-          <p className="text-primary">{description}</p>
+          <p className="text-primary sm:text-lg">{description}</p>
         </div>
       </Anchor>
     </NextLink>
